d7143e3b45d3059381472bfc5ad5448e42b71c43
Nuwan Rathnayake <x@nuwanr.com>
2023-04-22 20:53:44 +0530

    Arithmetic operators

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//arithmetic operators
+let num1 = 5
+let num2 = 10
+let num3 = 11
+
+let add = num1 + num2 + num3
+console.log('Add',add)
+
+let sub = num2 - num1
+console.log('Sub',sub)
+
+let mul = num1 * num2
+console.log('Mul',mul)
+
+let div = num2 / num1
+console.log('Div',div)
+
+let mod = num3 % num1
+console.log('Modulus',mod)
+
+let exp = num1 ** num2
+console.log('Exponent',exp)
+
+let inc = num1++
+console.log('Increment',inc)
+console.log('Incremented number',num1)
+
+let dec = --num2
+console.log('Decrement',dec)
+console.log('Decremented number',num2)
