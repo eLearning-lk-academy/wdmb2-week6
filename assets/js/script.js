c7329b6cbd70a5399884b3b73d68b5af8a83e271
Nuwan Rathnayake <x@nuwanr.com>
2023-04-29 17:44:24 +0530

    Arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
-const car = {
-    manufacture: "Toyota",
-    year: 2015,
-    model: "corolla",
-    age: function (){
-        return 2023-this.year
-    }
-}
-car.year = 2016
+const cars = ['Toyota', 'BMW', 'Benz']
 
-console.log(car['year'])
-console.log(car.model)
-console.log(car.age())
+console.log(cars)
+console.log(cars[2])
+
+cars[1] = 'Tesla'
+console.log(cars)
+
+
+cars[4] = 'Ford'
+console.log(cars)
+console.log(cars[3])
+
+document.getElementById('out').innerHTML = cars
+
+console.log(cars.toString())
+
+document.getElementById('out2').innerHTML = cars.join(' ')
+
+const cars2 = ['Honda', 'Nisan']
+
+const allCars = cars.concat(cars2)
+console.log(allCars)
+console.log(allCars.pop())
+console.log(allCars)
+console.log(allCars.push('Mini'))
+console.log(allCars)
+allCars.shift()
+console.log(allCars)
+allCars.unshift('Volovo')
+console.log(allCars)
+console.log(allCars.length)
+
+
